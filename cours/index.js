bcf09d9ed89d59b2bc9b1461f64f8fb932176d91
bdomat <bertrand.domat@gmail.com>
2023-02-13 22:49:16 +0100

    API progress

--- cours/index.js
+++ cours/index.js
@@ -1,5 +1,5 @@
 //XMLHTTPRequest
-
+/*
 function reqListener() {
   // console.log(this.responseText);
 }
@@ -26,4 +26,57 @@ fetch("data.txt").then((res) => res.text());
 
 fetch("data.json")
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => console.log(data));*/
+
+/*const myHeaders = new Headers();
+
+const init = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
+fetch("data.json", init).then((res) => console.log(res));
+*/
+//CRUD => Create (POST), read (GET), update (PUT), Delete (DELETE)
+const init2 = {
+  method: "DELETE",
+  headers: {
+    "Content-type": "application/json",
+  },
+  body: JSON.stringify({
+    pseudo: "From Scratch",
+    message: "Yo les gens !",
+  }),
+  mode: "cors",
+  credentials: "same-origin",
+};
+
+document.querySelector("form").addEventListener("submit", () => {
+  fetch("http://localhost:3000/posts", init2).then(() =>
+    console.log("data envoyée")
+  );
+});
+
+//--------------
+// Asynchrone
+//--------------
+
+setTimeout(() => {
+  console.log("test");
+}, 2000);
+
+//Promise
+fetch("monLien").then((res) => res);
+
+//async/await
+async function fetchData() {
+  await fetch("monlien");
+  //attend que le await soit executé avant de faire la suite
+  executeFunction();
+}
+
+const fetchData2 = async () => {
+  await fetch("monlien");
+  //attend que le await soit executé avant de faire la suite
+};
