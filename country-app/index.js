6a85ab2b68045df556990ba6640355f19b87c2c5
bdomat <bertrand.domat@gmail.com>
2023-02-22 22:32:18 +0100

    country-app finalization

--- country-app/index.js
+++ country-app/index.js
@@ -1,6 +1,8 @@
 let countries = [];
 const countriesCards = document.querySelector(".countries-container");
 const resultsNumber = document.getElementById(`inputRange`);
+const sortBtn = document.querySelectorAll(".btnSort");
+let sortMethod = "minToMax";
 
 async function fetchCountries() {
   await fetch(`https://restcountries.com/v3.1/all`)
@@ -11,12 +13,25 @@ async function fetchCountries() {
 }
 
 function countriesDisplay() {
+  rangeValue.innerHTML = `${inputRange.value}`;
   countriesCards.innerHTML = countries
     .filter((country) =>
       country.translations.fra.common
         .toLowerCase()
         .includes(inputSearch.value.toLowerCase())
     )
+    .sort((a, b) => {
+      if (sortMethod === "maxToMin") {
+        return b.population - a.population;
+      } else if (sortMethod === "minToMax") {
+        return a.population - b.population;
+      } else if (sortMethod === "alpha") {
+        return a.translations.fra.common.localeCompare(
+          b.translations.fra.common
+        );
+      }
+    })
+    .slice(0, inputRange.value)
     .map(
       (country) =>
         `
@@ -31,13 +46,17 @@ function countriesDisplay() {
   </div>
   `
     )
-    .join("")
-    .slice(4, inputRange);
+    .join("");
 }
 
 window.addEventListener("load", fetchCountries);
 inputSearch.addEventListener("input", countriesDisplay);
+inputRange.addEventListener("input", countriesDisplay);
 
-// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
-
+sortBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    sortMethod = `${e.target.id}`;
+    countriesDisplay();
+  });
+});
 // 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
